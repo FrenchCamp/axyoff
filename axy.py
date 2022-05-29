e7fe970b8168dea10ed457033c95f5282c90cfa7
FrenchCamp <entreprise.informatique92@gmail.com>
2022-05-29 16:33:00 +0200

    0.2-b modif

--- axy.py
+++ axy.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+ # Importation des modules
 from bs4 import BeautifulSoup
 import requests
 import os
@@ -9,8 +10,11 @@ from art import *
 
 sys.path.append('__init__')
 
+ # Rensaignement de l'adresse
 ip = input('[Scraping -> Adress] >>> ')
 os.system('clear')
+
+ # Main
 def main():
  chx = input('>>> ')
 tprint('Axy')
@@ -19,26 +23,31 @@ print(colored('-------------------------------------------', 'red'))
 
 print(colored('Use "help" for list of commands ', 'cyan'))
 
+ # Composants pour faire fonctionner l'application
 rep = requests.get(ip)
 soup = BeautifulSoup(rep.text, 'html.parser')
 chx = input('>>> ')
 
+ # Boucle infinie
 while True:
  
+  # 1 : Affiche le code
  if chx == 'text':
     print(soup)
     chx = input('>>> ')
 
- elif chx == 'cls':
-  os.system('clear')
-  chx = input('>>> ')
-
+  # 2 : Rechercher un élément dans le code
  elif chx == 'finder':
-    find = input('[Scraping -> Find element] >>> ')
+   find = input('[Scraping -> Find element] >>> ')
     item = soup.find_all(find)
     print(item)
     chx = input('>>> ')
 
+  # 3 : Efface 
+ elif chx == 'cls':
+    os.system('clear')
+
+  # 4 : Affiche l'aide et autres informations
  elif chx == 'help':
   print('----------')
   print(colored('Help', 'red'))
@@ -53,14 +62,17 @@ while True:
   print('Axy is a Web Scraping application developed by FrenchCamp.\nWebsite : https://frenchcamp.github.io/fr/axy.html ')
   chx = input('>>> ')
 
+  # 5 : Quitter l'application
  elif chx == '99':
    sys.exit()
- 
+
+  # 6 : Rapporter un bug
  elif chx == 'report':
   print('A bug, a problem? you can report it with the following email address:')
   print(colored('frenchcamp.axy.bug@gmail.com', 'blue'))
   chx = input('>>> ')
 
+  # Si autre commande éffectuée
  else:
   print(''+chx+': invalid command')
   chx = input('>>> ')
